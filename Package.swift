730254c03d6b2acbb72f80e5f8443e1380ca4df2
ci-macpaw <ci-macpaw@users.noreply.github.com>
2022-12-23 15:27:23 +0000

    chore: release 3.1.0

--- Package.swift
+++ Package.swift
@@ -12,11 +12,17 @@ let package = Package(
     .library(name: "Setapp", targets: ["SetappWrapper"]),
   ],
   targets: [
-    .target(name: "SetappWrapper",
-            dependencies: [.target(name: "Setapp"),
-                           .target(name: "SetappResources-iOS",
-                                   condition: .when(platforms: [.iOS]))]),
+    .target(
+        name: "SetappWrapper",
+        dependencies: [
+            .target(name: "Setapp"),
+            .target(
+                name: "SetappResources-iOS",
+                condition: .when(platforms: [.iOS])
+            )
+        ]
+    ),
     .binaryTarget(name: "Setapp", path: "Setapp.xcframework"),
-    .target(name: "SetappResources-iOS", resources: [.copy("Resources/Bundles/")]),
+    .target(name: "SetappResources-iOS", resources: [.copy("Resources/Bundles/SetappFramework-Resources-iOS.bundle")]),
   ]
 )
