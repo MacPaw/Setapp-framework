71a8834e772db271281c4a0dba285f0ef5d0366b
ci-macpaw <admin+ci-gh@macpaw.com>
2026-02-16 18:50:41 +0000

    chore: release 5.0.0

--- Package.swift
+++ Package.swift
@@ -1,43 +1,43 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
-  name: "Setapp",
-  platforms: [
-    .iOS(.v11),
-    .macOS(.v10_13),
-  ],
-  products: [
-    .library(
-        name: "Setapp",
-        targets: [
-            "SetappWrapper"
-        ]
-    ),
-  ],
-  targets: [
-    .target(
-        name: "SetappWrapper",
-        dependencies: [
-            .target(
-                name: "Setapp"
-            ),
-            .target(
-                name: "SetappResources-iOS",
-                condition: .when(platforms: [.iOS])
-            )
-        ]
-    ),
-    .binaryTarget(
-        name: "Setapp",
-        path: "Setapp.xcframework"
-    ),
-    .target(
-        name: "SetappResources-iOS",
-        resources: [
-            .copy("Resources/Bundles/SetappFramework-Resources-iOS.bundle")
-        ]
-    ),
-  ]
+    name: "Setapp",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v11),
+        .macCatalyst(.v15)
+    ],
+    products: [
+        .library(
+            name: "Setapp",
+            targets: [
+                "SetappWrapper"
+            ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SetappWrapper",
+            dependencies: [
+                .target(
+                    name: "Setapp"
+                ),
+                .target(
+                    name: "SetappResources"
+                )
+            ]
+        ),
+        .binaryTarget(
+            name: "Setapp",
+            path: "Setapp.xcframework"
+        ),
+        .target(
+            name: "SetappResources",
+            resources: [
+                .copy("Resources/Bundles/SetappFramework-Resources.bundle")
+            ]
+        )
+    ]
 )
